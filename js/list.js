ce598cd5bf3efd95e5c2796b7725b155a3300871
GRUBCAVE-WIN\Pharylon <me@zachshuford.com>
2019-10-06 09:55:16 -0400

    Square Cones

--- js/list.js
+++ js/list.js
@@ -9,10 +9,11 @@ export class Article {
  * @type {Article[]}
  */
 export const articleList = [
+    new Article("Square Cones", "squareCones.html", "2019-10-06"),
     new Article("D&D Beyond", "dndBeyond.html", "2019-08-08"),
-    new Article("WIP", "wip.html", "2019-08-08"),
-    new Article("Mobile", "mobile.html", "2018-12-26"),
-    new Article("More Blog Thoughts", "moreBlogThoughts.html", "2018-11-14"),
-    new Article("I Made A Blog", "article1.html", "2018-11-13"),
+    //new Article("WIP", "wip.html", "2019-08-08"),
+    //new Article("Mobile", "mobile.html", "2018-12-26"),
+    //new Article("More Blog Thoughts", "moreBlogThoughts.html", "2018-11-14"),
+    //new Article("I Made A Blog", "article1.html", "2018-11-13"),
     new Article("My Kids Are Cute", "myKids.html", "2018-11-12"),
 ];
