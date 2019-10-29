f34e4c3b8d938efecc0484288c1de08c107886d8
GRUBCAVE-WIN\Pharylon <me@zachshuford.com>
2019-10-29 14:54:35 -0400

    rearranged articles

--- js/list.js
+++ js/list.js
@@ -9,8 +9,9 @@ export class Article {
  * @type {Article[]}
  */
 export const articleList = [
+    new Article("D&D Beyond", "dndBeyond.html", "2019-10-20"),
+    new Article("Spellbook", "spellbook.html", "2019-10-18"),
     new Article("My Dream", "dream.html", "2019-10-06"),
-    new Article("D&D Beyond", "dndBeyond.html", "2019-08-08"),
     //new Article("WIP", "wip.html", "2019-08-08"),
     //new Article("Mobile", "mobile.html", "2018-12-26"),
     //new Article("More Blog Thoughts", "moreBlogThoughts.html", "2018-11-14"),
